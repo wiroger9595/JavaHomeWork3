edffb9ad207e8eaf5b858bec7ba811effe544408
Robert Chen <wiroger9595@gmail.com>
2020-06-02 18:35:26 +0800

    Add files via upload

---
+++ Homewrok_3.java
@@ -0,0 +1,103 @@
+package HomeWorkArea;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Homewrok_3 {
+
+	public static void main(String[] args) {
+		
+		//first question /////////////////////////////////////////////////////
+		// TODO Auto-generated method stub
+//		System.out.println("input number");
+//		Scanner scan1 = new Scanner(System.in);	
+//		Scanner scan2 = new Scanner(System.in);
+//		Scanner scan3 = new Scanner(System.in);
+//
+//		int newWord1 = scan1.nextInt();
+//		int newWord2 = scan2.nextInt();
+//		int newWord3 = scan3.nextInt();
+//
+//		//int newData = scan.nextInt();
+//		
+//		if((newWord1^2) + (newWord2^2) < (newWord3^2) || ((newWord3^2) + (newWord2^2) < (newWord1^2)) || ((newWord3^2) + (newWord1^2) < (newWord2^2))) {
+//			System.out.println("這不是三角形");
+//			
+//		} else {
+//			
+//			if(newWord1 == newWord2 && newWord3 == newWord2 && newWord3 == newWord1) {
+//				System.out.println("是正三角形");
+//			} 
+//			else if (newWord1 != newWord2 && newWord3 != newWord2 && newWord3 != newWord1){
+//				
+//				if ((newWord1^2) + (newWord2^2) == (newWord3^2) || ((newWord3^2) + (newWord2^2) == (newWord1^2)) || ((newWord3^2) + (newWord1^2) == (newWord2^2))) {
+//				System.out.println("是直角三角形");
+//				} else {
+//				
+//				System.out.println("是其他三角形");
+//				}
+//			}
+//			else  {
+//				System.out.println("是等腰三角形");
+//			}
+//		}
+		
+		
+		
+		
+		//second /////////////////////////////////////////////////////
+		
+		
+		
+//		int intRandom = (int)(Math.random()*101);
+//		
+//		
+//		System.out.println(intRandom);
+//		while(true) {
+//		
+//		System.out.println("input number");
+//		Scanner testIt = new Scanner(System.in);
+//		int newTest = testIt.nextInt();	
+//		
+//			if(intRandom != newTest) {
+//				System.out.println("guess wrong");
+//				System.out.println("next line");
+//				
+//				if(intRandom > newTest) {
+//					System.out.println("number is larger than your typing");
+//				} else {
+//					System.out.println("number is smaller than your typing");
+//
+//				}
+//				
+//			} else {
+//				System.out.println("you got the right number "+ intRandom);
+//				break;
+//			}
+//	
+//		}
+		
+
+		//third/////////////////////////////////////////////////////
+		
+		
+		int availableNumber = (int)(Math.random()*51);
+
+		System.out.println("don't show this number");
+		Scanner randomNumber = new Scanner(System.in);
+		
+		int hateNumber = randomNumber.nextInt();	
+		
+		
+		for(int i=0; i<50; i++) {
+//			if(Arrays.contains(i))) {
+//				
+//			}
+			
+		}
+		
+		
+	}
+
+}
